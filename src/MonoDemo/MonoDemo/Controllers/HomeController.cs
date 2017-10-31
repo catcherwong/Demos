89ad21d178c6f4da70ca124c8d973d4703576aee
catcherwong <catcher_hwq@outlook.com>
2017-10-31 22:12:26 +0800

    add asp.net core 2.0 via mono demo

---
+++ src/MonoDemo/MonoDemo/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonoDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("running asp.net core 2.0 via mono!");
+        }
+    }
+}
