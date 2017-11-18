a09e162383711623f8b6b0f34504360c669bda57
catcherwong <catcher_hwq@outlook.com>
2017-11-18 19:07:12 +0800

    :sparkles: Add simple APIGateway demo using Ocelot.

---
+++ src/APIGatewayDemo/ProductsAPIServices/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductsAPIServices.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Surface Book 2", "Mac Book Pro" };
+        }
+    }
+}
