2290b12f57d9beeb83dcf05179ee8950e2ec0501
catcherwong <catcher_hwq@outlook.com>
2018-03-17 09:12:28 +0800

    :sparkles: Add RoundRobinDemo.

---
+++ src/RoundRobinDemo/RoundRobinDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace RoundRobinDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lbUrls = new List<string>
+            {
+                "http://10.0.10.1/api/values",
+                "http://10.0.10.2/api/values",
+                "http://10.0.10.3/api/values",
+                "http://10.0.10.4/api/values",
+            };
+
+            var robin = new RoundRobin<string>(lbUrls);
+
+            var visitCount = lbUrls.Count * new Random().Next(3, 5);
+
+            Console.WriteLine("begin one by one..");
+            for (int i = 0; i < visitCount; i++)
+            {
+                Console.WriteLine($"{i + 1}:Sending request to {robin.GetNextItem()}");
+            }
+
+            Console.WriteLine("begin parallel..");
+            Parallel.For(0, visitCount, i =>
+            {
+                Console.WriteLine($"{i + 1}:Sending request to {robin.GetNextItem()}");
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/RoundRobinDemo/RoundRobinDemo/RoundRobin.cs
@@ -0,0 +1,35 @@
+﻿namespace RoundRobinDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RoundRobin<T>
+    {
+        private readonly IList<T> _items;
+        private readonly object _syncLock = new object();
+
+        private int _currentIndex = -1;
+
+        public RoundRobin(IEnumerable<T> sequence)
+        {
+            _items = sequence.ToList();
+
+            if(_items.Count <= 0 )
+            {
+                throw new ArgumentException("Sequence contains no elements.", nameof(sequence));
+            }                           
+        }
+
+        public T GetNextItem()
+        {
+            lock (this._syncLock)
+            {
+                _currentIndex++;
+                if (_currentIndex >= _items.Count)
+                    _currentIndex = 0;
+                return _items[_currentIndex];
+            }
+        }
+    }
+}
