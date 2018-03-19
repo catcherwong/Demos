d2eeab710a0b364cebb1f8beba4e32d57829f6a8
catcherwong <catcher_hwq@outlook.com>
2018-03-19 22:14:50 +0800

    :sparkles: Add WeightedRoundRobinDemo.

---
+++ src/WeightedRoundRobinDemo/WeightedRoundRobinDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace WeightedRoundRobinDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lbUrls = new Dictionary<string,int>
+            {
+                {"http://10.0.10.1/api/values",5},
+                {"http://10.0.10.2/api/values",1},
+                {"http://10.0.10.3/api/values",1},
+            };
+
+            var robin = new WeightedRoundRobin<string>(lbUrls);
+
+            var visitCount = lbUrls.Values.Sum() * new Random().Next(3, 5);
+
+            Console.WriteLine("begin one by one..");
+            for (int i = 0; i < visitCount; i++)
+            {
+                Console.WriteLine($"{i + 1}:Sending request to {robin.GetNextItem()}");
+            }
+
+            Console.WriteLine("begin parallel..");
+            Parallel.For(0, visitCount, i =>
+            {
+                Console.WriteLine($"{i + 1}:Sending request to {robin.GetNextItem()}");
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/WeightedRoundRobinDemo/WeightedRoundRobinDemo/WeightedRoundRobin.cs
@@ -0,0 +1,92 @@
+﻿namespace WeightedRoundRobinDemo
+{
+    using System.Collections.Generic;
+
+    public class WeightedRoundRobin<T>
+    {
+        /// <summary>
+        /// The server dict.
+        /// </summary>
+        private readonly IDictionary<T, int> _serverDict;
+        /// <summary>
+        /// The server list.
+        /// </summary>
+        private readonly IList<ServerConfig> _serverList = new List<ServerConfig>();
+        /// <summary>
+        /// The sync lock.
+        /// </summary>
+        private readonly object _syncLock = new object();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:WeightedRoundRobinDemo.WeightedRoundRobin`1"/> class.
+        /// </summary>
+        /// <param name="serverDict">Server dict.</param>
+        public WeightedRoundRobin(IDictionary<T, int> serverDict)
+        {
+            this._serverDict = serverDict;
+
+            foreach (var item in _serverDict)
+            {
+                _serverList.Add(new ServerConfig
+                {
+                    Current_Weight = 0,
+                    Weight = item.Value,
+                    Server = item.Key
+                });
+            }
+        }
+
+        /// <summary>
+        /// Gets the next item.
+        /// </summary>
+        /// <returns>The next item.</returns>
+        public T GetNextItem()
+        {
+            int index = -1;
+            int total = 0;
+            int size = _serverList.Count;
+
+            lock(_syncLock)
+            {             
+                for (int i = 0; i < size; i++)
+                {
+                    _serverList[i].Current_Weight += _serverList[i].Weight;
+                    total += _serverList[i].Weight;
+
+                    if (index == -1 || _serverList[index].Current_Weight < _serverList[i].Current_Weight)
+                    {
+                        index = i;
+                    }
+                }
+
+                _serverList[index].Current_Weight -= total;
+            }
+
+            return _serverList[index].Server;
+        }
+
+        /// <summary>
+        /// Server config.
+        /// </summary>
+        public class ServerConfig
+        {
+            /// <summary>
+            /// Gets or sets the weight.
+            /// </summary>
+            /// <value>The weight.</value>
+            public int Weight { get; set; }
+
+            /// <summary>
+            /// Gets or sets the current weight.
+            /// </summary>
+            /// <value>The current weight.</value>
+            public int Current_Weight { get; set; }
+
+            /// <summary>
+            /// Gets or sets the server.
+            /// </summary>
+            /// <value>The server.</value>
+            public T Server { get; set; }
+        }
+    }
+}
