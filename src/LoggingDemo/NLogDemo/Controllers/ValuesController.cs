d3137795ea31a4eed479800bfe3afed82a1bb222
catcherwong <catcher_hwq@outlook.com>
2018-04-18 23:20:13 +0800

    :sparkles: Add NLogDemo.

---
+++ src/LoggingDemo/NLogDemo/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿namespace NLogDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System.Collections.Generic;
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public ValuesController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ValuesController>();
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogDebug("debug");
+            _logger.LogError("error");
+            _logger.LogTrace("trace");
+            _logger.LogInformation("info");
+            _logger.LogWarning("warn");
+            _logger.LogCritical("critical");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ src/LoggingDemo/NLogDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace NLogDemo
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using NLog.Web;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseNLog()
+                .Build();
+    }
+}
