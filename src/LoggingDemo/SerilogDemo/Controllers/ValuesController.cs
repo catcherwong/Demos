ee72ff041204923c9317f8fd212402d8e8a2e57d
catcherwong <catcher_hwq@outlook.com>
2018-04-22 15:01:22 +0800

    :sparkles: Add SerilogDemo.

---
+++ src/LoggingDemo/SerilogDemo/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿namespace SerilogDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System.Collections.Generic;
+    
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public ValuesController(ILoggerFactory loggerFactory)
+        {
+            this._logger = loggerFactory.CreateLogger<ValuesController>();
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInformation("aaa");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ src/LoggingDemo/SerilogDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SerilogDemo
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;    
+    using Serilog;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                   .UseSerilog((hostingContext, loggerConfiguration) => 
+                               loggerConfiguration.WriteTo.File("demo.log",
+                                                                rollingInterval: RollingInterval.Day
+                                                               ))
+                .Build();
+    }
+}
