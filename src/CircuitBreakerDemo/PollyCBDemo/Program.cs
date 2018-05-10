7ac1ae9221d188cc414499684169e9c6ddd25085
catcherwong <catcher_hwq@outlook.com>
2018-05-10 23:01:06 +0800

    :construction: Combine Fallback and CircuitBreaker

--- src/CircuitBreakerDemo/PollyCBDemo/Program.cs
+++ src/CircuitBreakerDemo/PollyCBDemo/Program.cs
@@ -6,26 +6,19 @@ namespace PollyCBDemo
 {
     class Program
     {
-        private static PolicyWrap<string> policy;
-
-        public Program()
-        {
-            Action<Exception, TimeSpan> onBreak = (exception, timespan) => { Console.WriteLine("onbreak"); };
-            Action onReset = () => { Console.WriteLine("onreset"); };
-            var breaker = Policy
-                .Handle<Exception>()
-                .CircuitBreaker(2, TimeSpan.FromSeconds(1), onBreak, onReset);
-            
-            policy =
-                Policy<string>
+        static PolicyWrap<string> policy = Policy<string>
                     .Handle<Exception>()
-                    .Fallback("fallback",(x)=>
+                    .Fallback("fallback", (x) =>
                     {
                         Console.WriteLine("执行失败，返回Fallback");
                     })
-                    .Wrap(breaker);
-   
-        }
+            .Wrap(
+                Policy
+                .Handle<Exception>()
+                .CircuitBreaker(2, TimeSpan.FromSeconds(1),
+                                onBreak: (exception, timespan) => { Console.WriteLine("onbreak"); },
+                                onReset: () => { Console.WriteLine("onreset"); }));
+
 
         static void Main(string[] args)
         {            
@@ -35,8 +28,18 @@ namespace PollyCBDemo
             {
                 var res = policy.Execute(() =>
                 {
-                    throw new Exception("test");
+                    if(new Random().NextDouble()<0.3)
+                    {
+                        throw new Exception("test");
+                    }
+                    else
+                    {
+                        return "catcher";
+                    }
+
                 });
+                System.Threading.Thread.Sleep(1001);
+                Console.WriteLine(res);
                 i++;
             }
 
