0a6aa3ef1e677904fbd9c28fdeb99d319343f4e0
catcherwong <catcher_hwq@outlook.com>
2018-08-18 17:17:05 +0800

    :sparkles: Refit With HttpClientFactory.

---
+++ src/CallAPIsDemo/RefitClientApi/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿namespace RefitClientApi.Controllers
+{    
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IPersonsApi _api;
+
+        public ValuesController(IPersonsApi api)
+        {
+            this._api = api;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<List<Person>> GetAsync()
+        {
+            return await _api.GetPersonsAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<Person> GetAsync(int id)
+        {
+            return await _api.GetPersonAsync(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<Person> PostAsync([FromBody] Person value)
+        {
+            return await _api.AddPersonAsync(value);
+        }
+
+        // PUT api/values
+        [HttpPut]
+        public async Task<string> PutAsync([FromBody]int id)
+        {
+            return await _api.EditPersonAsync(id);
+        }
+
+        // DELETE api/values
+        [HttpDelete("id")]
+        public async Task<string> DeleteAsync(int id)
+        {
+            return await _api.DeletePersonAsync(id);
+        }
+    }
+}
---
+++ src/CallAPIsDemo/RefitClientApi/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace RefitClientApi
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Refit;
+    using Steeltoe.Common.Http.Discovery;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRefitClient<IPersonsApi>()
+                    .ConfigureHttpClient(options =>
+                    {
+                        options.BaseAddress = new Uri(Configuration.GetValue<string>("personapi_url"));
+                        //other settings of httpclient
+                    })
+                    //Steeltoe discovery
+                    //.AddHttpMessageHandler<DiscoveryHttpMessageHandler>()
+                    ;
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
