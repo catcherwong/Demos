8c1f1a395ba8c4142ec1cd4f670fd38c6d447696
catcherwong <catcher_hwq@outlook.com>
2018-12-03 22:28:21 +0800

    :sparkles: choise parameter.

--- src/Template/content/TplDemo/src/TplDemo.Data/DapperRepositoryBase.cs
+++ src/Template/content/TplDemo/src/TplDemo.Data/DapperRepositoryBase.cs
@@ -2,7 +2,16 @@
 {
     using Microsoft.Extensions.Configuration;
     using System.Data.Common;
+#if (MsSQL)
     using System.Data.SqlClient;
+#elif (MySQL)
+    using MySql.Data.MySqlClient;
+#elif (PgSQL)
+    using Npgsql;
+#else 
+    using Microsoft.Data.Sqlite;
+#endif
+
 
     public class DapperRepositoryBase
     {
@@ -23,17 +32,15 @@
         /// <returns>The db connection.</returns>
         protected DbConnection GetDbConnection()
         {
+#if (MsSQL)            
             return new SqlConnection(_connStr);
-        }
-
-        /// <summary>
-        /// Gets the db connection.
-        /// </summary>
-        /// <returns>The db connection.</returns>
-        /// <param name="connStr">Conn string.</param>
-        protected DbConnection GetDbConnection(string connStr)
-        {
-            return new SqlConnection(connStr);
-        }
+#elif (MySQL)            
+            return new MySqlConnection(_connStr);
+#elif (PgSQL)             
+            return new NpgsqlConnection(_connStr);
+#else              
+            return new SqliteConnection(_connStr);
+#endif              
+        }     
     }
 }
